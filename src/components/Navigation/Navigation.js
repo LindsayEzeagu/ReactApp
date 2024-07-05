f415a704f10823e3a10542bf2e85ca68ae3a97cb
UP960777 <74740661+UP960777@users.noreply.github.com>
2024-07-05 09:27:14 +0100

    created repo

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return(
+    <nav>
+      <img src="" alt="my  web log"/>
+
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
